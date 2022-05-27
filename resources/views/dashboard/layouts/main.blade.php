8dbe578abd69191ad74e9aa4210117e96f2b0c0a
nazhoir <nazhoirulamin031201@gmail.com>
2022-05-28 06:43:29 +0700

    update

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -21,70 +21,14 @@
   </head>
   <body>
     
-        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 fw-bold text-white" href="#">atBlog</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-        <div class="navbar-nav">
-            <div class="nav-item text-nowrap">
-                <form action="/logout" method="post" >
-                    @csrf
-                    <button type="submit" class="nav-link bg-dark border-0 text-white px-3">
-                      <i class="fa-solid fa-arrow-right-to-bracket"></i> Log Out</a>
-                    </button>
-                  </form>
-            </div>
-        </div>
-        </header>
+        @include('dashboard/partials/header')
 
         <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div class="position-sticky pt-3">
-                <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
-                        <i class="fa-solid fa-bars-progress"></i>
-                         Dashboard
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="fa-solid fa-file-pen"></i>
-                         Posts
-                    </a>
-                </li>
-                </ul>
-
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">Settings</h6>
-                <ul class="nav flex-column mb-2">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="fa-solid fa-sliders"></i> All Settings
-                    </a>
-                </li>
-                </ul>
-            </div>
-            </nav>
+            @include('dashboard/partials/sidebar')
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Dashboard</h1>
-                <div class="btn-toolbar mb-2 mb-md-0">
-                <div class="btn-group me-2">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-                </div>
-                <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                    <span data-feather="calendar" class="align-text-bottom"></span>
-                    This week
-                </button>
-                </div>
-            </div>
-                @yield('dashboard')
-            
+                @yield('dashboard') 
             </main>
         </div>
         </div>
